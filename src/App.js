a4bbe2869dad5f2331dc9e5a5de588630c1cd260
samrat <majumdersamrat26@gmail.com>
2022-11-10 09:37:36 +0530

    aster

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Cars from "./api/cars.json";
+import Car_details from "./components/Car_details";
+import { useRef } from "react";
 
 function App() {
+  const ref = useRef(null);
+  const scrollRight = () => {
+    ref.scrollX += 40;
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="app">
+        {Cars.map((item) => (
+          <Car_details
+            id={item.id}
+            type={item.bodyType}
+            name={item.modelName}
+            modelType={item.modelType}
+            image={item.imageUrl}
+          />
+        ))}
+      </div>
+      <div className="app-btn_container">
+        <button className="left-btn button">
+          <img src="./images/chevron-circled.svg" alt="Right Button" />
+        </button>
+        <button onClick={scrollRight} className="right-btn button">
+          <img src="./images/chevron-circled.svg" alt="Left Button" />
+        </button>
+      </div>
+    </>
   );
 }
 
